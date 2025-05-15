62939b79f91599c7a325bc45c942ffb63dd2e10a
PabloCarrai <pablohernanjuan@hotmail.com>
2025-05-15 17:09:26 -0300

    capaz que lo termino antes de lo pensado

--- ParaHacer.py
+++ ParaHacer.py
@@ -1,41 +1,50 @@
-tareas = ""
+import os
+tareas = []
+
+def limpiarPantalla():
+    os.system('clear')
+
+def listarTareas(tareas):
+    limpiarPantalla()
+    print("Tareas")
+    for i in range(len(tareas)):
+        print(f"{i}-{tareas[i]}")
 
 
-def listarTareas():
-    print("Las tareas son:")
-    print(f"{tareas}")
-
-
-def agregarTareas():
-    titulo = input("Ingrese su tarea")
+def agregarTareas(tareas):
+    limpiarPantalla()
+    titulo = input("Ingrese su tarea ")
+    tareas.append(titulo)
 
 
 def eliminarTareas():
+    limpiarPantalla()
     pass
 
 
 def salirPrograma():
-    print("Muchas gracias por usar este programa")
+    limpiarPantalla()
+    print("Muchas gracias por usar este programa ")
 
 
 def menu():
-    print("Bienvenido a Para-Hacer")
-    print("Elija una opcion:")
+    print("Bienvenido a Para-Hacer ")
+    print("Elija una opcion: ")
     print("""          
           1)Listar Tareas
           2)Agregar Tareas
           3)Eliminar Tareas          
           4)Salir del programa
           """)
-    desicion = int(input("Elija una opcion 1/2/3"))
+    desicion = int(input("Elija una opcion (1/2/3/4)   "))
     print(f"Su eleccion {desicion}")
     if (desicion == 4):
         salirPrograma()
     elif (desicion == 1):
-        listarTareas()
+        listarTareas(tareas)
         menu()
     elif (desicion == 2):
-        agregarTareas()
+        agregarTareas(tareas)
         menu()
 
 

049b39c36e1e7fe8afd300466ea7f99eaad5c4f1
PabloCarrai <pablohernanjuan@hotmail.com>
2025-05-14 21:28:46 -0300

    Habemus menu(no funcional)

---
+++ ParaHacer.py
@@ -0,0 +1,14 @@
+def menu():
+    print("Bienvenido a Para-Hacer")
+    print("Elija una opcion:")
+    print("""          
+          1)Listar Tareas
+          2)Agregar Tareas
+          3)Editar Tareas          
+          4)Salir del programa
+          """)
+    desicion = int(input("Elija una opcion 1/2/3"))
+    print(f"Su eleccion {desicion}")
+
+
+menu()

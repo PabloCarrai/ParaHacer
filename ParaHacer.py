7136df8cf355a2cc959525adb9ae067d28a0d9d3
PabloCarrai <pablohernanjuan@hotmail.com>
2025-05-18 19:55:15 -0300

    Hay que arreglar el flujo, y rehacer la eliminar tarea

--- ParaHacer.py
+++ ParaHacer.py
@@ -5,17 +5,32 @@ import os
 tareas = []
 
 #   Con esto limpio la pantalla(solo en ambiente gnu/linux)
+
+
 def limpiarPantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+def volverMenu():
+    volvemos = int(input("Regresamos al menu? 1=Si"))
+    if (volvemos == 1):
+        limpiarPantalla()
+        menu()
+
+
 #   Listamos las tareas y sus indices
+
+
 def listarTareas(tareas):
     limpiarPantalla()
     print("Tareas")
     for i in range(len(tareas)):
         print(f"{i}-{tareas[i]}")
+    volverMenu()
 
 #   Editamos las tareas por si las cargamos mal
+
+
 def editarTareas(tareas):
     listarTareas(tareas)
     indice = int(input("Ingrese el indice de la tarea a editar  "))
@@ -23,21 +38,23 @@ def editarTareas(tareas):
     tareas[indice] = editado
 
 #   Agregamos las tareas
+
+
 def agregarTareas(tareas):
     limpiarPantalla()
     titulo = input("Ingrese su tarea ")
     tareas.append(titulo)
 
 #   Eliminar Tareas
+
+
 def eliminarTareas(tareas):
-    limpiarPantalla()
-    listarTareas(tareas)
-    indice = int(input("Ingrese el numero de la tarea a eliminar "))
-    del tareas[indice]
-    listarTareas(tareas)
-    menu()
+    pass
+
 
 #   Salimos del programa
+
+
 def salirPrograma():
     limpiarPantalla()
     print("Muchas gracias por usar este programa ")
@@ -52,32 +69,37 @@ def salirPrograma():
             """)
 
 #   Menu del programa
+
+
 def menu():
-    print("Bienvenido a Para-Hacer ")
-    print("Elija una opcion: ")
-    print("""          
-          1) Listar Tareas
-          2) Agregar Tareas
-          3) Eliminar Tareas
-          4) Editar Tarea          
-          5) Salir del programa
+    print("""
+        +-----------------------------+
+        |    Bienvenido               |                
+        +-----------------------------+
+        | 1) Listar Tareas            | 
+        | 2) Agregar Tareas           |
+        | 3) Eliminar Tareas          |
+        | 4) Editar Tarea             |      
+        | 5) Salir del programa       |
+        +-----------------------------+
+        | Elija (1/2/3/4/5)           |   
+        +-----------------------------+
+
           """)
-    desicion = int(input("Elija una opcion (1/2/3/4/5)   "))
+    desicion = int(input(""))
     print(f"Su eleccion {desicion}")
     if (desicion == 5):
         salirPrograma()
     elif (desicion == 1):
         listarTareas(tareas)
-        menu()
     elif (desicion == 2):
         agregarTareas(tareas)
-        menu()
+        volverMenu()
     elif (desicion == 3):
         eliminarTareas(tareas)
-        menu()
     elif (desicion == 4):
         editarTareas(tareas)
-        menu()
+        volverMenu()
 
 
 #   Arrancamos el programa
